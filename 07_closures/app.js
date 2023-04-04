2a559033ef9f8cb78abcb13bcc296f79ebf2638d
lyamlim97 <lyamlim97@gmail.com>
2023-04-04 14:00:20 +0800

    add code for 07_closures

---
+++ 07_closures/app.js
@@ -0,0 +1,20 @@
+function pureFun(a, b) {
+  return a + b;
+}
+
+let b = 3;
+function impureFun(a) {
+  return a + b;
+}
+
+function outer() {
+  let x = 1;
+  function inner() {
+    x = x + 1;
+  }
+  return inner;
+}
+
+const incrementX = outer();
+incrementX();
+incrementX();
