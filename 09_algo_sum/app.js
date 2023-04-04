5beaf1edc2d96e6d227b67163350f489e5169c6b
lyamlim97 <lyamlim97@gmail.com>
2023-04-04 14:57:42 +0800

    add code for 09_algo_sum

---
+++ 09_algo_sum/app.js
@@ -0,0 +1,17 @@
+// Solution 1
+const sum = [1, 3, 5, 7].reduce((acc, cur) => acc + cur, 0);
+console.log(sum);
+
+// Solution 2
+export function cumSum(arr) {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    total += arr[i];
+  }
+
+  return total;
+}
+
+console.log("sum: ", cumSum([1, 3, 5, 7]));
